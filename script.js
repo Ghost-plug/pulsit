688555d48529560cf7d558fd6a7686b1f0fea8a0
Ghost-plug <danieldanielessien@gmail.com>
2025-02-27 13:06:26 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+/* script.js */
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("Landing page loaded successfully.");
+    document.getElementById("signup-form").addEventListener("submit", function(event) {
+        event.preventDefault();
+        window.location.href = "https://chat.whatsapp.com/J1SZufhEiq9GJ1RvpuCGvy";
+    });
+});
